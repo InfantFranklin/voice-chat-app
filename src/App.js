2440d944e283add8199cb889486d0f86c9a19ad6
InfantFranklin <infant.franklin1@gmail.com>
2024-09-21 12:35:07 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Join from "./components/Join";
+import ChatRoom from "./components/ChatRoom";
 
-function App() {
+const App = () => {
+  const [channel, setChannel] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {channel ? <ChatRoom channel={channel} /> : <Join onJoin={setChannel} />}
     </div>
   );
-}
+};
 
 export default App;
